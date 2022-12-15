c83671f5ad860d77bb21b28ebdeece8ae7ddf821
sinhaHarshit <81342501+sinha-harshit@users.noreply.github.com>
2022-12-15 21:54:29 +1100

    Implemented delete functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Todos from './components/Todos';
+import React, {useState} from 'react'
+
+
 
 function App() {
   const onDelete = (todo) => {
     console.log("Deleting this item", todo.title)
+
+    setTodos(todos.filter((t)=>{
+      return t!==todo;
+    }))
   }
 
-  let todos = [
+const [todos, setTodos] = useState([
     {
       sno: 1,
       title: "TODO 1",
@@ -25,7 +32,7 @@ function App() {
       title: "TODO 3",
       desc: "This is the Description of TODO 3"
     }
-  ]
+  ]);
   return (
     <>
     <Header title="Todo List"/>
